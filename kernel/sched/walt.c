febaf0952b3afc57edbaf85f9a71351468bb356a
v225hy <v225hy@users.noreply.github.com>
2026-09-13 14:18:07 +0800

    sched/walt: default walt_io_is_busy=1 (count iowait as busy; PowerHAL never writes it)

--- kernel/sched/walt.c
+++ kernel/sched/walt.c
@@ -39,7 +39,7 @@ static __read_mostly unsigned int walt_window_stats_policy =
 	WINDOW_STATS_MAX;
 static __read_mostly unsigned int walt_account_wait_time;
 static __read_mostly unsigned int walt_freq_account_wait_time = 0;
-unsigned int sysctl_sched_walt_io_is_busy = 0;
+unsigned int sysctl_sched_walt_io_is_busy = 1;
 
 unsigned int sysctl_sched_walt_init_task_load_pct = 15;
 
